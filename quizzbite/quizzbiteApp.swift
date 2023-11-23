b9f36512686ace6d8ec47692b384f2a6ba81869a
Sam Davies <samdavies170601@gmail.com>
2023-11-23 18:58:34 +0000

    Initial Commit

---
+++ quizzbite/quizzbiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizzbiteApp.swift
+//  quizzbite
+//
+//  Created by Sam Davies on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct quizzbiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
