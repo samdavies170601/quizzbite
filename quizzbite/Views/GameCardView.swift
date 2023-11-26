220ad634724c48fff20aa6439b7a5bb03e5ec2a6
Sam Davies <samdavies170601@gmail.com>
2023-11-26 18:18:51 +0000

    Fix Game.description text to use multiple lines

--- quizzbite/Views/GameCardView.swift
+++ quizzbite/Views/GameCardView.swift
@@ -16,48 +16,52 @@ struct GameCardView: View {
         ZStack {
             Color.gray.ignoresSafeArea() //temporary
             
-            
             ZStack {
                 RoundedRectangle(cornerRadius: 10.0)
                     .foregroundStyle(.white)
                 
-                //Game Stack
-                VStack {
+                // MARK: - Start of Card Content VStack
+                VStack(spacing: 0) {
                     
-                    //Game.name Stack
                     HStack {
-                        Text(game.name).fontWeight(.semibold).font(.system(size: 18.0)) //need to import a custom font and sort out font sizing and font weight
+                        // TODO: Import the custom font, Poppins, create an AppFont ViewModel and implement the font within each View.
+                        Text(game.name).fontWeight(.semibold).font(.system(size: 18.0))
                         Spacer()
                     }
+                    //.border(.black)
                     
-                    //Game.description Stack
                     HStack {
-                        Text(game.description).fontWeight(.regular).font(.system(size: 12.0)) //need to sort out the available space for the description text to use
+                        Text(game.description).fontWeight(.regular).font(.system(size: 12.0))
                             .foregroundStyle(.gray)
+                            .frame(height: 30)
                         Spacer()
                     }
+                    //.border(.black)
                     
                     Spacer()
                     
-                    //Game.highScore Stack
                     HStack {
-                        Text("High Score: \(game.highScore30)").fontWeight(.medium).font(.system(size: 14.0)) //depends on the output of the duration selection button
+                        // TODO: Implement a duration button to change the visible high score for each duration.
+                        Text("High Score: \(game.highScore30)").fontWeight(.medium).font(.system(size: 14.0))
                         Spacer()
                     }
-                    
+                    //.border(.black)
+            
                 }
                 .padding(16.0)
+                //.border(.black)
+                // MARK: - End of Card Content VStack
                 
             }
             .aspectRatio(3.05, contentMode: .fit)
             .frame(maxWidth: 366.0, maxHeight: 120.0)
             .padding(.horizontal, 32.0)
             
-            
         }
     }
 }
 
+// TODO: Learn the new preview system.
 #Preview {
     GameCardView(game: Game(name: "Guess the Flag", description: "Test your knowledge of World Flags in this quiz: Guess the Flag!", highScore30: 0, highScore60: 0, highScore90: 0))
 }
