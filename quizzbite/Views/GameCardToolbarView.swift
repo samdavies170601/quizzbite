b1e5ceec3766842b97dd38702de7302cabd7cb93
Sam Davies <samdavies170601@gmail.com>
2023-12-01 18:21:40 +0000

    Setup GameCardToolbarView without custom assets

--- quizzbite/Views/GameCardToolbarView.swift
+++ quizzbite/Views/GameCardToolbarView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct GameCardToolbarView: View {
+    
+    @State private var durationSelected = GameLogic.GameDuration.shortGame
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 32) {
+            
+            HStack {
+                
+                //Duration Selector
+                ForEach(GameLogic.GameDuration.allCases, id: \.self) { duration in
+                    Button(action: {
+                        durationSelected = duration
+                    }, label: {
+                        Text("\(duration.rawValue)")
+                        // TODO: Implement AppColor to set the specific colour of a selected and unselected duration.
+                            .foregroundStyle(duration == durationSelected ? .blue : .gray)
+                    })
+                }
+                
+            }
+            
+            //Quick Play Button
+            Button(action: {
+                // TODO: Navigates to CountdownView and initialises the game.
+            }, label: {
+                // TODO: Implement the correct icon.
+                Image(systemName: "play")
+            })
+            
+        }
     }
 }
 
