f882a6a9a7ac729a315c6d8f8f27a2397e12b933
Sam Davies <samdavies170601@gmail.com>
2023-12-01 18:22:15 +0000

    Create GameLogic ViewModel and GameDuration enumeration within GameLogic

--- quizzbite/ViewModels/GameLogic.swift
+++ quizzbite/ViewModels/GameLogic.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+class GameLogic {
+    
+    ///The duration of a quizzbite game.
+    enum GameDuration: Int, CaseIterable {
+        case shortGame = 30
+        case mediumGame = 60
+        case longGame = 90
+    }
+    
+}
