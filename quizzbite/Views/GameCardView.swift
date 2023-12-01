cb83bf0ff8d47d2710a24726b851a389a7a4e289
Sam Davies <samdavies170601@gmail.com>
2023-12-01 17:42:45 +0000

    Refactor the contents of GameCardView in an extension of GameCardView

--- quizzbite/Views/GameCardView.swift
+++ quizzbite/Views/GameCardView.swift
@@ -16,49 +16,58 @@ struct GameCardView: View {
         ZStack {
             Color.gray.ignoresSafeArea() //temporary
             
-            ZStack {
-                RoundedRectangle(cornerRadius: 10.0)
-                    .foregroundStyle(.white)
-                
-                // MARK: - Start of Card Content VStack
-                VStack(spacing: 0) {
-                    
-                    HStack {
-                        // TODO: Import the custom font, Poppins, create an AppFont ViewModel and implement the font within each View.
-                        Text(game.name).fontWeight(.semibold).font(.system(size: 18.0))
-                        Spacer()
-                    }
-                    //.border(.black)
-                    
-                    HStack {
-                        Text(game.description).fontWeight(.regular).font(.system(size: 12.0))
-                            .foregroundStyle(.gray)
-                            .frame(height: 30)
-                        Spacer()
-                    }
-                    //.border(.black)
-                    
-                    Spacer()
-                    
-                    HStack {
-                        // TODO: Implement a duration button to change the visible high score for each duration.
-                        Text("High Score: \(game.highScore30)").fontWeight(.medium).font(.system(size: 14.0))
-                        Spacer()
-                    }
-                    //.border(.black)
-            
-                }
-                .padding(16.0)
-                //.border(.black)
-                // MARK: - End of Card Content VStack
-                
-            }
-            .aspectRatio(3.05, contentMode: .fit)
-            .frame(maxWidth: 366.0, maxHeight: 120.0)
-            .padding(.horizontal, 32.0)
+            gameCardContent
             
         }
     }
+}
+
+extension GameCardView {
+    
+    //Game Card Content
+    var gameCardContent: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .foregroundStyle(.white)
+            
+            // MARK: - Start of Card Content VStack
+            VStack(spacing: 0) {
+                
+                HStack {
+                    // TODO: Import the custom font, Poppins, create an AppFont ViewModel and implement the font within each View.
+                    Text(game.name).fontWeight(.semibold).font(.system(size: 18.0))
+                    Spacer()
+                }
+                //.border(.black)
+                
+                HStack {
+                    Text(game.description).fontWeight(.regular).font(.system(size: 12.0))
+                        .foregroundStyle(.gray)
+                        .frame(height: 30)
+                    Spacer()
+                }
+                //.border(.black)
+                
+                Spacer()
+                
+                HStack {
+                    // TODO: Implement a duration button to change the visible high score for each duration.
+                    Text("High Score: \(game.highScore30)").fontWeight(.medium).font(.system(size: 14.0))
+                    Spacer()
+                }
+                //.border(.black)
+        
+            }
+            .padding(16.0)
+            //.border(.black)
+            // MARK: - End of Card Content VStack
+            
+        }
+        .aspectRatio(3.05, contentMode: .fit)
+        .frame(maxWidth: 366.0, maxHeight: 120.0)
+        .padding(.horizontal, 32.0)
+    }
+    
 }
 
 // TODO: Learn the new preview system.
