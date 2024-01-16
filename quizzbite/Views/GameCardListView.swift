1b5bec625dcb3f57018f68599e13ad6f9c3768d1
Sam Davies <samdavies170601@gmail.com>
2024-01-16 13:42:35 +0000

    Change formatting

---
+++ quizzbite/Views/GameCardListView.swift
@@ -0,0 +1,18 @@
+//
+//  GameCardListView.swift
+//  quizzbite
+//
+//  Created by Sam Davies on 15/01/2024.
+//
+
+import SwiftUI
+
+struct GameCardListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameCardListView()
+}
